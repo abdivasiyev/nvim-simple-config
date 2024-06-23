ce099eff9f47e5ed3acdb4eb9fd659c5c19d849a
Asliddinbek Azizovich <46614434+abdivasiyev@users.noreply.github.com>
2024-06-23 18:07:29 +0500

    fix: disable netrw after installation of NerdTree

--- lua/remap.lua
+++ lua/remap.lua
@@ -3,7 +3,8 @@ vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader><F5>', vim.cmd.UndotreeToggle)
 
 -- back to directory tree
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
 -- move line up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
